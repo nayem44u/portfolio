93169b20a3d583870dddfdbc7182483d330dfa02
nayem44u <106361950+nayem44u@users.noreply.github.com>
2022-05-27 08:44:46 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var typed = new Typed(".typing",{
+    strings:["","Wed Developer","","Programmer","","FullStack Dev"],
+    typespeed:100,
+    BackSpeed:01,
+    loop:true
+});
+
+function alertbox(){
+    alert("Your Message is sent");
+}
